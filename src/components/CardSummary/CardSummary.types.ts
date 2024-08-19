d378df0f2cc1c6bbbc276ece51329a3692b930b9
Nightmare <fajardoariel435@gmail.com>
2024-08-19 23:02:09 +0200

    s

--- src/components/CardSummary/CardSummary.types.ts
+++ src/components/CardSummary/CardSummary.types.ts
@@ -2,7 +2,7 @@ import { LucideIcon } from "lucide-react"
 
 export type CardSummaryProps = {
     icon: LucideIcon
-    total: string | number
+    total: string | Promise<number>
     average: number
     title: string
     tooltipText: string
