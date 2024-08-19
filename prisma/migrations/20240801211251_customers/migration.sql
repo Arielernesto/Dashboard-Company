047556db58591f513ee266d3405c22bcead8940c
Nightmare <fajardoariel435@gmail.com>
2024-08-20 01:06:07 +0200

    aser

--- prisma/migrations/20240801211251_customers/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Customer" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "amount" TEXT NOT NULL,
-    "status" TEXT NOT NULL DEFAULT '0'
-);
